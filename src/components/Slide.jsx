6a6852546e01d316a3fd26ae39f4c3a0fc439c37
Jonas Saraiva Jr <jonas.saraivajr@gmail.com>
2023-09-01 18:17:30 -0300

    fix: slide component, fix: navbar, add: favicon

--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -1,43 +1,55 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 
 import "swiper/css";
-import "swiper/css/effect-coverflow";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
-
-import { Autoplay, EffectCoverflow } from "swiper/modules";
 
 import starIcon from "../assets/star-icon.svg";
+
 import { TrendingItems } from "../constants";
 
+import { useEffect, useState } from "react";
+
 // Componente para exibição de slides
-const Slide = ({ slides, effect, space }) => {
+const Slide = () => {
+  // Estado para controlar o número de slides visíveis com base no tamanho da tela
+  const [slides, setSlides] = useState(1);
+
+  // Efeito para atualizar o número de slides visíveis com base no redimensionamento da tela
+  useEffect(() => {
+    const handleResize = () => {
+      const width = window.innerWidth;
+      if (width >= 1024) {
+        setSlides(3);
+      } else if (width >= 600) {
+        setSlides(2);
+      } else {
+        setSlides(1);
+      }
+    };
+
+    // Chama a função handleResize para definir o número de slides iniciais com base no tamanho da tela
+    handleResize();
+    // Adiciona um ouvinte de evento de redimensionamento da tela para atualizar os slides conforme necessário
+    window.addEventListener("resize", handleResize);
+
+    // Remove o ouvinte de evento de redimensionamento quando o componente é desmontado
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div className="flex w-full justify-center">
       {/* Inicialização do componente Swiper */}
       <Swiper
-        effect={effect ? "coverflow" : ""}
-        coverflowEffect={{
-          rotate: 0,
-          stretch: 100,
-          depth: 0,
-          slideShadows: false,
-        }}
-        modules={[Autoplay, EffectCoverflow]}
         slidesPerView={slides}
-        spaceBetween={space}
-        loop={true}
-        autoplay={{
-          delay: 10000,
-          disableOnInteraction: false,
-        }}
+        spaceBetween={-40}
         className="flex w-full justify-center"
       >
         {/* Mapeamento dos itens do Trending */}
         {TrendingItems.map((item) => (
           // Slide individual
           <SwiperSlide key={item.id} className="relative">
-            <div className="w-3/4 max-w-[420px] min-[768px]:w-full min-[768px]:max-w-[420px]">
+            <div className="w-3/4 max-w-[420px]">
               <img src={item.img} alt="" className="w-full h-auto" />
             </div>
 
